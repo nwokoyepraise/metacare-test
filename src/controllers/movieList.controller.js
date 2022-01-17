bb8245c3239eab5b6a30c2b378448662f2e8eb45
nwokoyepraise <nwokoyepraise@gmail.com>
2022-01-17 13:48:28 +0100

    select only relevant fetch data

--- src/controllers/movieList.controller.js
+++ src/controllers/movieList.controller.js
@@ -4,7 +4,18 @@ module.exports = async function () {
     try {
         let url = `https://swapi.py4e.com/api/films/`;
         let params = {};
-        return await fetchHelper(url, params);
+        
+
+        //fetch movie data
+        let data = await fetchHelper(url, params);
+        let movies = [];
+
+        //sort and select only relevant data
+        data.results.forEach(element => {
+            movies.push({ title: element.title, episode_id: element.episode_id, opening_crawl: element.opening_crawl, release_date: element.release_date, url: element.url })
+        });
+        
+        return {count: data.count, movies};
     } catch (error) {
         console.error(error);
     }
--- src/utils/fetchHelper.js
+++ src/utils/fetchHelper.js
@@ -3,7 +3,7 @@ const axios = require('axios').default;
 
 module.exports = async function makeRequest(url, params) {
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(url, params);
       return response.data
     } catch (error) {
       console.error(error);
